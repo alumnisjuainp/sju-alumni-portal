2b168db1e1ca291cce73ab7c11103f1181fe72e8
gyaan-09 <gyaannluthria@gmail.com>
2026-04-26 20:47:55 +0530

    Added default route to prevent Not Found error

--- backend/server.js
+++ backend/server.js
@@ -37,6 +37,11 @@ app.use(cors({
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
+// Default Route
+app.get('/', (req, res) => {
+    res.send('SJU Alumni Portal API is running!');
+});
+
 // Routes
 app.use('/api/health', require('./routes/healthRoutes'));
 app.use('/api/alumni', alumniRoutes);
